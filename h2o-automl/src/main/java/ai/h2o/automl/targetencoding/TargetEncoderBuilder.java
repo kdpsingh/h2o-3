0f817aa669a14cb5e087572efe78e5df5b01f5fe
Andrey Spiridonov <andrey@h2o.ai>
2019-05-29 17:28:17 +0200

    PUBDEV-6255: Reenabling tests and some code polishing.

--- h2o-automl/src/main/java/ai/h2o/automl/targetencoding/TargetEncoderBuilder.java
+++ h2o-automl/src/main/java/ai/h2o/automl/targetencoding/TargetEncoderBuilder.java
@@ -2,7 +2,7 @@ package ai.h2o.automl.targetencoding;
 
 import hex.ModelBuilder;
 import hex.ModelCategory;
-import water.H2O;
+import water.DKV;
 
 public class TargetEncoderBuilder extends ModelBuilder<TargetEncoderModel, TargetEncoderModel.TargetEncoderParameters, TargetEncoderModel.TargetEncoderOutput> {
 
@@ -16,22 +16,15 @@ public class TargetEncoderBuilder extends ModelBuilder<TargetEncoderModel, Targe
     public void computeImpl() {
       // Nothing,  but later we can perform creation of encoding map here
       TargetEncoderModel targetEncoderModel = new TargetEncoderModel(_job._result, _parms,  new TargetEncoderModel.TargetEncoderOutput(TargetEncoderBuilder.this));
-      targetEncoderModel.delete_and_lock(_job);
-      targetEncoderModel.update(_job);
-      targetEncoderModel.unlock(_job);
+      DKV.put(targetEncoderModel);
     }
   }
   
   @Override
   protected Driver trainModelImpl() {
     // We can use Model.Parameters to configure Target Encoder
-//    throw H2O.unimpl("Model is supposed to be pretrained");
     return new TargetEncoderDriver();
   }
-  
-//  public TargetEncoderModel getModel() {
-//    return new TargetEncoderModel(_parms);
-//  }
   
   @Override
   public ModelCategory[] can_build() {
